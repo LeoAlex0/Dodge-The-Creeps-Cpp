5029f14fcee365d6f03ad10aa2fe34bc4d6bcfa5
zLeoAlex <z.alex.leo@outlook.com>
2020-02-27 16:49:42 +0800

    Code reformat

--- src/Player.cpp
+++ src/Player.cpp
@@ -7,8 +7,9 @@
 using namespace godot;
 
 void Player::_input( const Ref<InputEvent> event ) {
-    if (event->is_class("InputEventScreenTouch") && cast_to<InputEventScreenTouch>(event.ptr())->is_pressed()){
-        _target = cast_to<InputEventScreenTouch>(event.ptr())->get_position();
+    if ( event->is_class( "InputEventScreenTouch" ) &&
+         cast_to<InputEventScreenTouch>( event.ptr() )->is_pressed() ) {
+        _target = cast_to<InputEventScreenTouch>( event.ptr() )->get_position();
     }
 }
 
@@ -17,17 +18,17 @@ void Player::_process( const float delta ) {
     const Input &   input       = *Input::get_singleton();
     AnimatedSprite &animatedSpirite =
         *(AnimatedSprite *) get_node( "AnimatedSprite" );
-//    Vector2 vel( input.is_action_pressed( "ui_right" ) -
-//                     input.is_action_pressed( "ui_left" ),
-//                 input.is_action_pressed( "ui_down" ) -
-//                     input.is_action_pressed( "ui_up" ) );
+    //    Vector2 vel( input.is_action_pressed( "ui_right" ) -
+    //                     input.is_action_pressed( "ui_left" ),
+    //                 input.is_action_pressed( "ui_down" ) -
+    //                     input.is_action_pressed( "ui_up" ) );
 
     Vector2 vel = _target - get_position();
 
     if ( vel.length() > delta * Speed ) {
         vel = vel.normalized() * Speed;
 
-        set_rotation(vel.angle()+Math_PI/2);
+        set_rotation( vel.angle() + Math_PI / 2 );
         animatedSpirite.play();
     } else {
         animatedSpirite.stop();
@@ -41,13 +42,13 @@ void Player::_process( const float delta ) {
     // Set
     set_position( position );
 
-//    if ( vel.x != 0 ) {
-//        animatedSpirite.set_animation( "right" );
-//        animatedSpirite.set_flip_v( false );
-//        animatedSpirite.set_flip_h( vel.x < 0 );
-//    }
-//    if ( vel.y != 0 ) {
-//        animatedSpirite.set_animation( "up" );
-//        animatedSpirite.set_flip_v( vel.y > 0 );
-//    }
+    //    if ( vel.x != 0 ) {
+    //        animatedSpirite.set_animation( "right" );
+    //        animatedSpirite.set_flip_v( false );
+    //        animatedSpirite.set_flip_h( vel.x < 0 );
+    //    }
+    //    if ( vel.y != 0 ) {
+    //        animatedSpirite.set_animation( "up" );
+    //        animatedSpirite.set_flip_v( vel.y > 0 );
+    //    }
 }
