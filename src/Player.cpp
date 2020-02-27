60d8b046ec5692358b575ee332caa75dbd2313cb
zLeoAlex <z.alex.leo@outlook.com>
2020-02-27 16:47:50 +0800

    Android support

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,22 +1,33 @@
 #include "Player.hpp"
 #include <AnimatedSprite.hpp>
 #include <Input.hpp>
+#include <InputEvent.hpp>
+#include <InputEventScreenTouch.hpp>
 
 using namespace godot;
+
+void Player::_input( const Ref<InputEvent> event ) {
+    if (event->is_class("InputEventScreenTouch") && cast_to<InputEventScreenTouch>(event.ptr())->is_pressed()){
+        _target = cast_to<InputEventScreenTouch>(event.ptr())->get_position();
+    }
+}
 
 void Player::_process( const float delta ) {
     const Vector2   screen_size = get_viewport_rect().size;
     const Input &   input       = *Input::get_singleton();
     AnimatedSprite &animatedSpirite =
         *(AnimatedSprite *) get_node( "AnimatedSprite" );
-    Vector2 vel( input.is_action_pressed( "ui_right" ) -
-                     input.is_action_pressed( "ui_left" ),
-                 input.is_action_pressed( "ui_down" ) -
-                     input.is_action_pressed( "ui_up" ) );
+//    Vector2 vel( input.is_action_pressed( "ui_right" ) -
+//                     input.is_action_pressed( "ui_left" ),
+//                 input.is_action_pressed( "ui_down" ) -
+//                     input.is_action_pressed( "ui_up" ) );
 
-    if ( vel.length_squared() > 0 ) {
+    Vector2 vel = _target - get_position();
+
+    if ( vel.length() > delta * Speed ) {
         vel = vel.normalized() * Speed;
 
+        set_rotation(vel.angle()+Math_PI/2);
         animatedSpirite.play();
     } else {
         animatedSpirite.stop();
@@ -30,13 +41,13 @@ void Player::_process( const float delta ) {
     // Set
     set_position( position );
 
-    if ( vel.x != 0 ) {
-        animatedSpirite.set_animation( "right" );
-        animatedSpirite.set_flip_v( false );
-        animatedSpirite.set_flip_h( vel.x < 0 );
-    }
-    if ( vel.y != 0 ) {
-        animatedSpirite.set_animation( "up" );
-        animatedSpirite.set_flip_v( vel.y > 0 );
-    }
+//    if ( vel.x != 0 ) {
+//        animatedSpirite.set_animation( "right" );
+//        animatedSpirite.set_flip_v( false );
+//        animatedSpirite.set_flip_h( vel.x < 0 );
+//    }
+//    if ( vel.y != 0 ) {
+//        animatedSpirite.set_animation( "up" );
+//        animatedSpirite.set_flip_v( vel.y > 0 );
+//    }
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -4,6 +4,7 @@
 #include "Vector2.hpp"
 #include <Area2D.hpp>
 #include <Godot.hpp>
+#include <InputEvent.hpp>
 
 class Player : public godot::Area2D {
     GODOT_CLASS( Player, godot::Area2D );
@@ -16,11 +17,14 @@ class Player : public godot::Area2D {
         register_property( "speed", &Player::Speed, 400 );
 
         register_method( "_process", &Player::_process );
+        register_method( "_input", &Player::_input );
     }
 
-    int Speed = 400;
+    int            Speed   = 400;
+    godot::Vector2 _target = godot::Vector2( 0, 0 );
 
     void _process( const float delta );
+    void _input( const godot::Ref<godot::InputEvent> event );
 };
 
 #endif
